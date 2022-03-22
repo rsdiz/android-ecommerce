4bab310e40551a769372a838b125823d1d963e1b
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2022-03-22 19:52:49 +0700

    (:domain) Add Dependency Injection Module
    Signed-off-by: Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>

---
+++ domain/src/main/java/id/rsdiz/rdshop/domain/di/RepositoryModule.kt
@@ -0,0 +1,36 @@
+package id.rsdiz.rdshop.domain.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import id.rsdiz.rdshop.data.di.NetworkModule
+import id.rsdiz.rdshop.domain.repository.auth.AuthRepository
+import id.rsdiz.rdshop.domain.repository.auth.IAuthRepository
+import id.rsdiz.rdshop.domain.repository.category.CategoryRepository
+import id.rsdiz.rdshop.domain.repository.category.ICategoryRepository
+import id.rsdiz.rdshop.domain.repository.order.IOrderRepository
+import id.rsdiz.rdshop.domain.repository.order.OrderRepository
+import id.rsdiz.rdshop.domain.repository.product.IProductRepository
+import id.rsdiz.rdshop.domain.repository.product.ProductRepository
+import id.rsdiz.rdshop.domain.repository.user.IUserRepository
+import id.rsdiz.rdshop.domain.repository.user.UserRepository
+
+@Module(includes = [NetworkModule::class])
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    abstract fun bindAuthRepository(repository: AuthRepository): IAuthRepository
+
+    @Binds
+    abstract fun bindUserRepository(repository: UserRepository): IUserRepository
+
+    @Binds
+    abstract fun bindCategoryRepository(repository: CategoryRepository): ICategoryRepository
+
+    @Binds
+    abstract fun bindProductRepository(repository: ProductRepository): IProductRepository
+
+    @Binds
+    abstract fun bindOrderRepository(repository: OrderRepository): IOrderRepository
+}
